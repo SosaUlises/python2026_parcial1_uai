843f6c8b7b8c64517a2b878790a16ade881d2590
SosaUlises <ulipvp@gmail.com>
2026-06-19 17:48:29 -0300

    Problema 3

---
+++ problema3.py
@@ -0,0 +1,10 @@
+def main():
+    palabras = ["hola", "mundo", "python"]
+    nueva_lista = []
+    
+    for palabra in palabras:
+        nueva_lista.append(palabra.upper())
+    
+    print(nueva_lista)
+
+main()
